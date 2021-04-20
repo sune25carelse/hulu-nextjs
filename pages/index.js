6e9dbe4d03f438dca7b44712722894646fb3f5c5
Sune Carelse <sune.carelse@takealot.com>
2021-04-20 17:38:26 +0200

    app/style yarn run dev

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <h1>Lets build Hulu 2.0</h1>
     </div>
-  )
+  );
 }
