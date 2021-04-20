a057ee66caa230d43df31583a1ea8926775f52e8
Sune Carelse <sune.carelse@takealot.com>
2021-04-20 18:11:01 +0200

    icons rendered yarn add heroicons

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,21 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react";
+
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
     <header className="">
-      <h1>This is the header</h1>
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
