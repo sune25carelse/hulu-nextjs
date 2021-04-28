1af175308097b0d175fb6c0111f03178a11e9001
Sune Carelse <sune.carelse@takealot.com>
2021-04-28 19:40:56 +0200

    update

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     <nav>
       <div
         className="flex px-10 sm:px-20 text-2xl whitespace-nowrap 
-      space-x-10 sm:space-x-20 overflow-x-scroll "
+      space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"
       >
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
