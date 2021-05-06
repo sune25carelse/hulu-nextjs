30a74c5cd8a7664b9f50be2078d5fd73a54669f7
Sune Carelse <sune.carelse@takealot.com>
2021-05-06 21:50:21 +0200

    route of the movie genre

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,9 @@
 import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 function Nav() {
+  const router = useRouter();
+
   return (
     <nav className="relative">
       <div
@@ -10,6 +13,7 @@ function Nav() {
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
+            onClick={() => router.push(`/?genre=${key}`)}
             className="last:pr-24 cursor-pointer transition duration-100 
             transform hover:scale-125 
             hover:text-white active:text-red-500"
