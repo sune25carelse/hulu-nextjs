41a9aac2031473e67f7b0473fb066802daecc134
Sune Carelse <sune.carelse@takealot.com>
2021-05-06 22:16:43 +0200

    now we get data from tmdb

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
-export default function Home() {
+
+export default function Home(props) {
+  console.log(props);
   return (
     <div>
       <Head>
@@ -26,4 +28,10 @@ export async function getServerSideProps(context) {
       requests[genre]?.url || requests.fetchTrending.url
     }`
   ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request,
+    },
+  };
 }
