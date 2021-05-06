2dd0f351e24091c6380fd0ee375af0fbc4a90738
Sune Carelse <sune.carelse@takealot.com>
2021-05-06 22:13:05 +0200

    resultsto tmdb api serverSide to get the genre

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
-
+import Results from "../components/Results";
+import requests from "../utils/requests";
 export default function Home() {
   return (
     <div>
@@ -12,6 +13,17 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
 }
