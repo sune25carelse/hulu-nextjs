d0aa5197041aa6505e8de61e4e79053ab7200f42
Sune Carelse <sune.carelse@takealot.com>
2021-06-22 21:22:54 +0200

    grid cols when going to bigger screen

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,7 @@ import Thumbnail from "./Thumbnail";
 
 function Results({ results }) {
   return (
-    <div>
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
