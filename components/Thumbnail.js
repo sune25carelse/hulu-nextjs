fbffc2414ba75f5a8185abdeec074a6e0140ff40
Sune Carelse <sune.carelse@takealot.com>
2021-06-22 21:03:29 +0200

    lazy loading images out da box, and p tags of name

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,19 +1,26 @@
-import Image from 'next/image';
+import Image from "next/image";
+import { ThumbUpIcon } from "@heroicons/react/outline";
 
 function Thumbnail({ result }) {
-
-
-  const BASE_URL ="https://image.tmdb.org/t/p/original/";
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
     <div>
-<Image
-layout='responsive'
-src={}
-height={1080}
-width={1920}
-/>
-</div>
+      <Image
+        layout="responsive"
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
+        }
+        height={1080}
+        width={1920}
+      />
+      <div className="p-2">
+        <p className="truncate max-w-md">{result.overview}</p>
+        <h2>{result.title || result.original_name}</h2>
+        <p></p>
+      </div>
+    </div>
   );
 }
 
