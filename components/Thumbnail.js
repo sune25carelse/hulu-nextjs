cdece3d4041691d5c071858ef8b4588d2eb22084
Sune Carelse <sune.carelse@takealot.com>
2021-06-22 21:10:44 +0200

    now see thumbUp release date mvie vote count etc

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -18,7 +18,12 @@ function Thumbnail({ result }) {
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
         <h2>{result.title || result.original_name}</h2>
-        <p></p>
+        <p>
+          {result.media_type && `${result.media_type} .`}
+          {""}
+          {result.release_date || result.first_air_date} .{""}
+          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+        </p>
       </div>
     </div>
   );
